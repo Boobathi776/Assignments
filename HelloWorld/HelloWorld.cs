99b55f7cd64d8ef565a8991411cd1662cc62a7c8
Boobathi776 <boobathir72@gmail.com>
2025-04-22 15:14:58 +0530

    tried one thing

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -7,6 +7,16 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Another obj = new Another();
+            obj.print("Boobathi");
+        }
+    }
+
+    class Another
+    {
+        public void print(string name)
+        {
+            Console.WriteLine($"hi {name}!");
         }
     }
 }
