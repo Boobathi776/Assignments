d088064e94b6cf01d0cfcddea3a8b93d6b65ab7f
Boobathi776 <boobathir72@gmail.com>
2025-04-23 10:17:50 +0530

    commands added

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace HelloWorld   //to make a package or container like structure of our classes and namespaces
 {
     class HelloWorld
     {
         static void Main(string[] args)
         {
+            //Hello World! Program
             Console.WriteLine("Hello World!");
+
+            //String passing to another class's method
             Another obj = new Another();
             obj.print("Boobathi");
+
+            //get input from the user
             obj.print(Console.ReadLine());
 
+            //Calculator program
             Calculator calc = new Calculator();
-           int sum =  calc.Add(5, 3);
-            Console.WriteLine(sum);
+            int sum =  calc.Add(5, 3);
+            Console.WriteLine("sum is : "+sum);
         }
     }
 
@@ -21,7 +27,7 @@ namespace HelloWorld
     {
         public void print(string name)
         {
-          
+            //String interpolation "$" is used to done like this 
             Console.WriteLine($"hi {name}!");
         }
     }
