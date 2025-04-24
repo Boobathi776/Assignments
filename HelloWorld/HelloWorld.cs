f532a09b41fdc93f8c0ef7986bb2eb48fc55c492
Boobathi776 <boobathir72@gmail.com>
2025-04-24 10:00:55 +0530

    third commit for calculator

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -9,26 +9,10 @@ namespace HelloWorld   //to make a package or container like structure of our cl
             //Hello World! Program
             Console.WriteLine("Hello World!");
 
-            //String passing to another class's method
-            Another obj = new Another();
-            obj.print("Boobathi");
-
-            //get input from the user
-            obj.print(Console.ReadLine());
-
+        
             //Calculator program
             Calculator calc = new Calculator();
-            int sum =  calc.Add(5, 3);
-            Console.WriteLine("sum is : "+sum);
-        }
-    }
-
-    class Another
-    {
-        public void print(string name)
-        {
-            //String interpolation "$" is used to done like this 
-            Console.WriteLine($"hi {name}!");
+            calc.callCalculator();
         }
     }
 }
